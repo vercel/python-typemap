ec99558f326d0ae9cb5051e43908342181f62b54
Michael J. Sullivan <sully@msully.net>
2025-11-06 11:08:00 -0800

    Start lifting over unions and add missing string functions

--- tests/test_type_eval.py
+++ tests/test_type_eval.py
@@ -5,11 +5,15 @@ from typing import Literal
 from typemap.typing import (
     NewProtocol,
     Member,
+    GetAttr,
     GetName,
     GetType,
     Iter,
     Attrs,
     Is,
+    Uppercase,
+    StrConcat,
+    StrSlice,
 )
 from typemap.type_eval import eval_typing
 
@@ -89,3 +93,69 @@ def test_eval_types_3():
         class F[bool]:
             fff: bool
         """)
+
+
+class TA:
+    x: int
+    y: list[float]
+    z: TB
+
+
+class TB:
+    x: str
+    y: list[object]
+
+
+def test_type_getattr_union_1():
+    d = eval_typing(GetAttr[TA | TB, Literal["x"]])
+    assert d == int | str
+
+
+def test_type_getattr_union_2():
+    d = eval_typing(GetAttr[TA, Literal["x"] | Literal["y"]])
+    assert d == int | list[float]
+
+
+def test_type_getattr_union_3():
+    d = eval_typing(GetAttr[TA | TB, Literal["x"] | Literal["y"]])
+    assert d == int | list[float] | str | list[object]
+
+
+def test_type_getattr_union_4():
+    d = eval_typing(GetAttr[TA, Literal["x", "y"]])
+    assert d == int | list[float]
+
+
+def test_type_getattr_union_5():
+    d = eval_typing(GetAttr[TA, Literal["x", "y"] | Literal["z"]])
+    assert d == int | list[float] | TB
+
+
+def test_type_strings_1():
+    d = eval_typing(Uppercase[Literal["foo"]])
+    assert d == Literal["FOO"]
+
+
+def test_type_strings_2():
+    d = eval_typing(Uppercase[Literal["foo", "bar"]])
+    assert d == Literal["FOO"] | Literal["BAR"]
+
+
+def test_type_strings_3():
+    d = eval_typing(StrConcat[Literal["foo"], Literal["bar"]])
+    assert d == Literal["foobar"]
+
+
+def test_type_strings_4():
+    d = eval_typing(StrConcat[Literal["a", "b"], Literal["c", "d"]])
+    assert d == Literal["ac"] | Literal["ad"] | Literal["bc"] | Literal["bd"]
+
+
+def test_type_strings_5():
+    d = eval_typing(StrSlice[Literal["abcd"], Literal[0], Literal[1]])
+    assert d == Literal["a"]
+
+
+def test_type_strings_6():
+    d = eval_typing(StrSlice[Literal["abcd"], Literal[1], Literal[None]])
+    assert d == Literal["bcd"]
