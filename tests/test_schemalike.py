932bf7ac5a93574674f421fb6ff0c6a9b1e2343c
Michael J. Sullivan <sully@msully.net>
2026-01-09 17:08:49 -0800

    Add a test for generating get_... methods

---
+++ tests/test_schemalike.py
@@ -0,0 +1,79 @@
+import textwrap
+
+from typing import Callable, Literal
+
+from typemap.type_eval import eval_typing
+from typemap.typing import (
+    NewProtocol,
+    Iter,
+    Attrs,
+    GetType,
+    GetName,
+    Member,
+    Param,
+    StrConcat,
+)
+
+from . import format_helper
+
+
+class Schema:
+    pass
+
+
+class Type:
+    pass
+
+
+class Expression:
+    pass
+
+
+# hmmmm... recursion with this sort of thing will be funny...
+# how will we handle the decorators or __init_subclass__ or what have you
+
+
+class Property:
+    name: str
+    required: bool
+    multi: bool
+    typ: Type
+    expr: Expression | None
+
+
+type Schemaify[T] = NewProtocol[
+    *[p for p in Iter[Attrs[T]]],
+    *[
+        Member[
+            StrConcat[Literal["get_"], GetName[p]],
+            Callable[
+                [
+                    Param[Literal["self"], Schemaify[T]],
+                    Param[Literal["schema"], Schema, Literal["keyword"]],
+                ],
+                GetType[p],
+            ],
+            Literal["ClassVar"],
+        ]
+        for p in Iter[Attrs[T]]
+    ],
+]
+
+
+def test_schema_like_1():
+    tgt = eval_typing(Schemaify[Property])
+    fmt = format_helper.format_class(tgt)
+
+    assert fmt == textwrap.dedent("""\
+    class Schemaify[tests.test_schemalike.Property]:
+        name: str
+        required: bool
+        multi: bool
+        typ: tests.test_schemalike.Type
+        expr: tests.test_schemalike.Expression | None
+        def get_name(self: Schemaify[tests.test_schemalike.Property], *, schema: tests.test_schemalike.Schema) -> str: ...
+        def get_required(self: Schemaify[tests.test_schemalike.Property], *, schema: tests.test_schemalike.Schema) -> bool: ...
+        def get_multi(self: Schemaify[tests.test_schemalike.Property], *, schema: tests.test_schemalike.Schema) -> bool: ...
+        def get_typ(self: Schemaify[tests.test_schemalike.Property], *, schema: tests.test_schemalike.Schema) -> tests.test_schemalike.Type: ...
+        def get_expr(self: Schemaify[tests.test_schemalike.Property], *, schema: tests.test_schemalike.Schema) -> tests.test_schemalike.Expression | None: ...
+    """)
