222ae346dfe3a34db478a4e304f09aeca4a1cc70
dnwpark <dnwpark@protonmail.com>
2026-01-26 15:35:44 -0800

    Add Session, select, Query, QueryRow.

--- tests/test_qblike_3.py
+++ tests/test_qblike_3.py
@@ -4,18 +4,18 @@ import textwrap
 
 from typing import (
     Any,
-    Callable,
     ForwardRef,
     Literal,
     Never,
     ReadOnly,
-    Self,
     TypedDict,
+    Unpack,
 )
 
-from typemap.type_eval import eval_typing
+from typemap.type_eval import eval_call_with_types, eval_typing
 from typemap.typing import (
     Attrs,
+    Length,
     GetArg,
     GetAttr,
     GetName,
@@ -26,7 +26,6 @@ from typemap.typing import (
     Iter,
     Member,
     NewProtocol,
-    Param,
     IsSub,
 )
 
@@ -67,10 +66,16 @@ Protocols are generated using AddInit[T], Create[T], and Update[T].
 """
 
 
+# Type Helpers
+
+
 type ReplaceNever[T, D] = T if not IsSub[T, Never] else D
-type GetFieldItem[T: InitField, K, Default] = ReplaceNever[
+type GetInitFieldItem[T: InitField, K, Default] = ReplaceNever[
     GetAttr[GetArg[T, InitField, Literal[0]], K], Default
 ]
+
+
+# Database Types
 
 
 @dataclasses.dataclass(frozen=True)
@@ -96,7 +101,7 @@ class Table[name: str]:
 
 
 class Field[Table, PyType]:
-    def __lt__(self, other: Any) -> Filter: ...
+    def __lt__(self, other: Any) -> Filter[Table]: ...
 
 
 type FieldTable[T] = GetArg[GetType[T], Field, Literal[0]]
@@ -116,36 +121,31 @@ class column[Args: ColumnArgs](InitField[Args]):
     pass
 
 
-type FieldIsNullable[Init] = GetFieldItem[
+type ColumnInitIsNullable[Init] = GetInitFieldItem[
     Init, Literal["nullable"], Literal[True]
 ]
-type FieldIsAutoincrement[Init] = GetFieldItem[
+type ColumnInitIsAutoincrement[Init] = GetInitFieldItem[
     Init, Literal["autoincrement"], Literal[False]
 ]
-type FieldHasDefault[Init] = (
+type ColumnInitHasDefault[Init] = (
     Literal[True]
-    if not IsSub[GetFieldItem[Init, Literal["default"], Never], Never]
+    if not IsSub[GetInitFieldItem[Init, Literal["default"], Never], Never]
+    else Literal[False]
+)
+
+type FieldValueNeverNull[F, C] = (
+    Literal[True]
+    if not IsSub[Literal[True], ColumnInitIsNullable[C]]
+    or IsSub[Literal[True], ColumnInitIsAutoincrement[C]]
+    or (
+        IsSub[FieldPyType[F], list]
+        and IsSub[GetArg[FieldPyType[F], list, Literal[0]], Table]
+    )
     else Literal[False]
 )
 
 
-type FieldIsRequiredForCreate[Init] = (
-    Literal[True]
-    if not IsSub[Literal[True], FieldIsNullable[Init]]
-    and not IsSub[Literal[True], FieldIsAutoincrement[Init]]
-    and not IsSub[Literal[True], FieldHasDefault[Init]]
-    else Literal[False]
-)
-type FieldIsDefaultNone[Init] = (
-    Literal[True]
-    if IsSub[Literal[True], FieldIsNullable[Init]]
-    and not IsSub[Literal[True], FieldIsAutoincrement[Init]]
-    and IsSub[GetFieldItem[Init, Literal["default"], None], None]
-    else Literal[False]
-)
-
-
-class Filter:
+class Filter[T: Table]:
     pass
 
 
@@ -172,57 +172,10 @@ class DbLinkSource[Args: DbLinkSourceArgs](InitField[Args]):
     pass
 
 
-class Default:
-    pass
+# Query Types
 
 
-type InitFnType[T] = Member[
-    Literal["__init__"],
-    Callable[
-        [
-            Param[Literal["self"], Self],
-            *[
-                Param[
-                    GetName[p],
-                    (
-                        FieldPyType[p]
-                        if IsSub[
-                            Literal[True], FieldIsRequiredForCreate[GetInit[p]]
-                        ]
-                        else FieldPyType[p] | None
-                        if IsSub[Literal[True], FieldIsDefaultNone[GetInit[p]]]
-                        else FieldPyType[p] | Default
-                    ),
-                    (
-                        Literal["keyword"]
-                        if IsSub[
-                            Literal[True], FieldIsRequiredForCreate[GetInit[p]]
-                        ]
-                        else Literal["keyword", "default"]
-                    ),
-                ]
-                for p in Iter[Attrs[T]]
-                if not IsSub[
-                    GetFieldItem[GetInit[p], Literal["db_type"], Never],
-                    DbLinkSource,
-                ]
-            ],
-        ],
-        None,
-    ],
-    Literal["ClassVar"],
-]
-type AddInit[T] = NewProtocol[
-    InitFnType[T],
-    *[Member[GetName[p], FieldPyType[p], GetQuals[p]] for p in Iter[Attrs[T]]],
-]
-
-
-class NoChange:
-    pass
-
-
-type Create[T] = NewProtocol[
+type Select[T] = NewProtocol[
     *[
         Member[
             GetName[p],
@@ -230,40 +183,64 @@ type Create[T] = NewProtocol[
                 FieldPyType[p]
                 if IsSub[
                     Literal[True],
-                    FieldIsRequiredForCreate[GetInit[p]],
+                    FieldValueNeverNull[GetType[p], GetInit[p]],
                 ]
                 else FieldPyType[p] | None
-                if IsSub[
-                    Literal[True],
-                    FieldIsDefaultNone[GetInit[p]],
-                ]
-                else FieldPyType[p] | Default
             ),
             GetQuals[p],
         ]
         for p in Iter[Attrs[T]]
-        if not IsSub[
-            Literal[True],
-            GetFieldItem[GetInit[p], Literal["primary_key"], Never],
-        ]
-        and not IsSub[
-            GetFieldItem[GetInit[p], Literal["db_type"], Never], DbLinkSource
-        ]
     ],
 ]
-type Update[T] = NewProtocol[
-    *[
-        Member[GetName[p], FieldPyType[p] | NoChange, GetQuals[p]]
-        for p in Iter[Attrs[T]]
-        if not IsSub[
-            Literal[True],
-            GetFieldItem[GetInit[p], Literal["primary_key"], Never],
+
+
+type AddTable[Tables, New] = (
+    Tables
+    if any(IsSub[t, New] and IsSub[New, t] for t in Iter[Tables])
+    else tuple[*[t for t in Iter[Tables]], New]
+)
+type AddTables[Tables, News] = (
+    Tables
+    if IsSub[Length[News], Literal[0]]
+    else AddTables[
+        AddTable[Tables, GetArg[News, tuple, Literal[0]]],
+        tuple[*([n for n in Iter[News]][1:])],
+    ]
+)
+type UniqueTables[Tables] = AddTables[tuple[()], Tables]
+
+
+def select[*E](
+    *entity: Unpack[E],
+) -> Query[UniqueTables[tuple[*[e for e in Iter[E]]]]]: ...
+
+
+class Query[E: tuple[type[Table], ...]]:
+    pass
+
+
+type QueryRow[E: tuple[type[Table], ...]] = (
+    Select[GetArg[E, tuple, Literal[0]]]
+    if IsSub[Literal[1], Length[E]]
+    else NewProtocol[
+        *[
+            Member[
+                Literal[e.__name__],
+                Select[e],
+            ]
+            for e in Iter[E]
         ]
-        and not IsSub[
-            GetFieldItem[GetInit[p], Literal["db_type"], Never], DbLinkSource
-        ]
-    ],
-]
+    ]
+)
+
+
+class Session:
+    def execute[E: tuple[type[Table], ...]](
+        self, query: Query[E]
+    ) -> list[QueryRow[E]]: ...
+
+
+# Application Types
 
 
 class User(Table[Literal["users"]]):
@@ -277,7 +254,9 @@ class User(Table[Literal["users"]]):
         db_type=DbString(length=100), unique=True, nullable=False
     )
     age: Field[User, int | None] = column(db_type=DbInteger())
-    active: Field[User, bool] = column(db_type=DbBoolean(), default=True)
+    active: Field[User, bool] = column(
+        db_type=DbBoolean(), default=True, nullable=False
+    )
     posts: Field[User, list[Post]] = column(
         db_type=DbLinkSource(source="Post", cardinality=Cardinality.MANY)
     )
@@ -313,79 +292,91 @@ class Comment(Table[Literal["comments"]]):
     )
 
 
-def test_qblike_3_add_init_01():
-    tgt = eval_typing(AddInit[User])
-    fmt = format_helper.format_class(tgt)
+# Tests
+
+
+def test_qblike_3_select_01():
+    # select(User)
+    query = eval_call_with_types(select, User)
+    fmt = format_helper.format_class(query)
 
     assert fmt == textwrap.dedent("""\
-        class AddInit[tests.test_qblike_3.User]:
+        class Query[tuple[tests.test_qblike_3.User]]:
+    """)
+
+    results = eval_call_with_types(Session.execute, Session, query)
+    result = eval_typing(GetArg[results, list, Literal[0]])
+    fmt = format_helper.format_class(result)
+    assert fmt == textwrap.dedent("""\
+        class Select[tests.test_qblike_3.User]:
             id: int
             name: str
             email: str
             age: int | None
             active: bool
             posts: list[tests.test_qblike_3.Post]
-            def __init__(self: Self, *, id: int | tests.test_qblike_3.Default = ..., name: str, email: str, age: int | None = ..., active: bool | tests.test_qblike_3.Default = ...) -> None: ...
     """)
 
 
-def test_qblike_3_add_init_02():
-    tgt = eval_typing(AddInit[Post])
-    fmt = format_helper.format_class(tgt)
+def test_qblike_3_select_02():
+    # select(User, User)
+    query = eval_call_with_types(select, User, User)
+    fmt = format_helper.format_class(query)
 
     assert fmt == textwrap.dedent("""\
-        class AddInit[tests.test_qblike_3.Post]:
+        class Query[tuple[tests.test_qblike_3.User]]:
+    """)
+
+    results = eval_call_with_types(Session.execute, Session, query)
+    result = eval_typing(GetArg[results, list, Literal[0]])
+    fmt = format_helper.format_class(result)
+    assert fmt == textwrap.dedent("""\
+        class Select[tests.test_qblike_3.User]:
+            id: int
+            name: str
+            email: str
+            age: int | None
+            active: bool
+            posts: list[tests.test_qblike_3.Post]
+    """)
+
+
+def test_qblike_3_select_03():
+    # select(User, Post)
+    query = eval_call_with_types(select, User, Post)
+    fmt = format_helper.format_class(query)
+
+    assert fmt == textwrap.dedent("""\
+        class Query[tuple[tests.test_qblike_3.User, tests.test_qblike_3.Post]]:
+    """)
+
+    results = eval_call_with_types(Session.execute, Session, query)
+    result = eval_typing(GetArg[results, list, Literal[0]])
+    fmt = format_helper.format_class(result)
+    assert fmt == textwrap.dedent("""\
+        class QueryRow[tuple[tests.test_qblike_3.User, tests.test_qblike_3.Post]]:
+            User: tests.test_qblike_3.Select[tests.test_qblike_3.User]
+            Post: tests.test_qblike_3.Select[tests.test_qblike_3.Post]
+    """)
+
+    result_user = eval_typing(GetAttr[result, Literal["User"]])
+    fmt = format_helper.format_class(result_user)
+    assert fmt == textwrap.dedent("""\
+        class Select[tests.test_qblike_3.User]:
+            id: int
+            name: str
+            email: str
+            age: int | None
+            active: bool
+            posts: list[tests.test_qblike_3.Post]
+    """)
+
+    result_post = eval_typing(GetAttr[result, Literal["Post"]])
+    fmt = format_helper.format_class(result_post)
+    assert fmt == textwrap.dedent("""\
+        class Select[tests.test_qblike_3.Post]:
             id: int
             content: str
             author: tests.test_qblike_3.User
             comments: list[tests.test_qblike_3.Comment]
-            def __init__(self: Self, *, id: int | tests.test_qblike_3.Default = ..., content: str, author: tests.test_qblike_3.User) -> None: ...
-    """)
-
-
-def test_qblike_3_create_01():
-    tgt = eval_typing(Create[User])
-    fmt = format_helper.format_class(tgt)
-
-    assert fmt == textwrap.dedent("""\
-        class Create[tests.test_qblike_3.User]:
-            name: str
-            email: str
-            age: int | None
-            active: bool | tests.test_qblike_3.Default
-    """)
-
-
-def test_qblike_3_create_02():
-    tgt = eval_typing(Create[Post])
-    fmt = format_helper.format_class(tgt)
-
-    assert fmt == textwrap.dedent("""\
-        class Create[tests.test_qblike_3.Post]:
-            content: str
-            author: tests.test_qblike_3.User
-    """)
-
-
-def test_qblike_3_update_01():
-    tgt = eval_typing(Update[User])
-    fmt = format_helper.format_class(tgt)
-
-    assert fmt == textwrap.dedent("""\
-        class Update[tests.test_qblike_3.User]:
-            name: str | tests.test_qblike_3.NoChange
-            email: str | tests.test_qblike_3.NoChange
-            age: int | None | tests.test_qblike_3.NoChange
-            active: bool | tests.test_qblike_3.NoChange
-    """)
-
-
-def test_qblike_3_update_02():
-    tgt = eval_typing(Update[Post])
-    fmt = format_helper.format_class(tgt)
-
-    assert fmt == textwrap.dedent("""\
-        class Update[tests.test_qblike_3.Post]:
-            content: str | tests.test_qblike_3.NoChange
-            author: tests.test_qblike_3.User | tests.test_qblike_3.NoChange
     """)
