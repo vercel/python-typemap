0f4d4c0f5d99caaf21f39d4661b4ed39dfc60947
Michael J. Sullivan <sully@msully.net>
2026-01-29 12:06:51 -0800

    Rename pre-pep.rst to pep.rst. (#62)
    It is still a draft but it's a draft of a PEP and not of a "pre-PEP".

--- scripts/update-examples.sh
+++ scripts/update-examples.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 scripts/py2rst.py tests/test_qblike_2.py --start "Begin PEP section" --end "End PEP section" \
-  | scripts/rst_replace_section.py pre-pep.rst qb-impl -i
+  | scripts/rst_replace_section.py pep.rst qb-impl -i
 
 
 scripts/py2rst.py tests/test_fastapilike_2.py --start "Begin PEP section" --end "End PEP section" \
-  | scripts/rst_replace_section.py pre-pep.rst init-impl -i
+  | scripts/rst_replace_section.py pep.rst init-impl -i
