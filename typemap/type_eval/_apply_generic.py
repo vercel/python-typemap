4fa4dc3fcc75c4021d5c3f354a66b9f33cf356d0
dnwpark <dnwpark@protonmail.com>
2026-02-09 16:28:49 -0800

    Pass receiver type to generic lambda.

--- typemap/type_eval/_apply_generic.py
+++ typemap/type_eval/_apply_generic.py
@@ -347,8 +347,9 @@ def get_local_defns(
             if stuck and stuff.__type_params__:
                 type_params = stuff.__type_params__
                 str_args = boxed.str_args
+                canonical_cls = boxed.canonical_cls
 
-                def _make_lambda(fn, o, sa, tp):
+                def _make_lambda(fn, o, sa, tp, cls):
                     from ._eval_operators import _function_type_from_sig
 
                     def lam(*vs):
@@ -362,14 +363,16 @@ def get_local_defns(
                         )
                         sig = _resolved_function_signature(fn, args)
                         return _function_type_from_sig(
-                            sig, o, receiver_type=None
+                            sig, o, receiver_type=cls
                         )
 
                     return lam
 
                 gc = GenericCallable[  # type: ignore[valid-type,misc]
                     tuple[*type_params],  # type: ignore[valid-type]
-                    _make_lambda(stuff, orig, str_args, type_params),
+                    _make_lambda(
+                        stuff, orig, str_args, type_params, canonical_cls
+                    ),
                 ]
                 annos[name] = typing.ClassVar[gc]
             elif local_fn is not None:
