cfac3aec39d75b3dc90ae7625c27dc5d12278f2b
Michael J. Sullivan <sully@msully.net>
2026-03-03 11:00:51 -0800

    Allow update-examples.sh to take an optional PEP filename argument

--- scripts/update-examples.sh
+++ scripts/update-examples.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+PEP=${1:-pep.rst}
+
 scripts/py2rst.py tests/test_qblike_2.py --start "Begin PEP section" --end "End PEP section" \
-  | scripts/rst_replace_section.py pep.rst pep827-qb-impl -i
+  | scripts/rst_replace_section.py "$PEP" pep827-qb-impl -i
 
 
 scripts/py2rst.py tests/test_dataclass_like.py --start "Begin PEP section: dataclass like" --end "End PEP section" \
-  | scripts/rst_replace_section.py pep.rst pep827-init-impl -i
+  | scripts/rst_replace_section.py "$PEP" pep827-init-impl -i
 
 scripts/py2rst.py tests/test_fastapilike_2.py --start "Begin PEP section: Automatically deriving FastAPI CRUD models" --end "End PEP section" \
-  | scripts/rst_replace_section.py pep.rst pep827-fastapi-impl -i
+  | scripts/rst_replace_section.py "$PEP" pep827-fastapi-impl -i
 
 scripts/py2rst.py tests/test_nplike.py --start "Begin PEP section" --end "End PEP section" \
-  | scripts/rst_replace_section.py pep.rst pep827-numpy-impl -i
+  | scripts/rst_replace_section.py "$PEP" pep827-numpy-impl -i
